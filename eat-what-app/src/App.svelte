29a090f03cfbd28d6465b9a5b2ac8bd13e2b989f
YUMING MA <DavidYangPersonal@outlook.com>
2021-07-14 18:31:36 +0800

    小优化

--- eat-what-app/src/App.svelte
+++ eat-what-app/src/App.svelte
@@ -1,61 +1,61 @@
 <script>
-	import Header from "./components/Header.svelte";
-	import CardContainer from "./components/CardContainer.svelte";
+  import Header from './components/Header.svelte';
+  import CardContainer from './components/CardContainer.svelte';
 </script>
 
 <div class="root">
-	<div class="nes">
-		<!-- <Header /> -->
-		<main>
-			<section class="title">
-				<h1>¿ 吃什么 ?</h1>
-				<div>
-					<img
-					src="https://gitee.com/yxon123/gitee-sources/raw/master//Images/2344124.jpg"
-					alt="Steak"
-					class="nes-avatar is-rounded is-large"
-					style="image-rendering: pixelated;"
-				/>
-				</div>
-			</section>
-			<CardContainer />
-		</main>
-	</div>
+  <div class="nes">
+    <!-- <Header /> -->
+    <main>
+      <section class="title">
+        <h1>¿ 吃什么 ?</h1>
+        <div>
+          <img
+            src="https://gitee.com/yxon123/gitee-sources/raw/master//Images/food-blogger.png"
+            alt="Steak"
+            class="nes-avatar is-rounded is-large"
+            style="image-rendering: pixelated;"
+          />
+        </div>
+      </section>
+      <CardContainer />
+    </main>
+  </div>
 </div>
 
 <style>
-	@media screen and (max-width: 2100px) {
-        .nes{
-            width:520px;
-        }
+  @media screen and (max-width: 2100px) {
+    .nes {
+      width: 540px;
     }
-	@media screen and (max-width: 600px) {
-        .nes{
-            width:360px;
-        }
-	}
-	@font-face {
-		font-family: "zpix";
-		src: url("./fonts/zpix-v1.2.1-legacy.ttf");
-	}
-	.root {
-		display: flex;
-		justify-content: center;
-		/* width: 100%; */
-	}
-	.nes {
-		font-family: "zpix";
-		color: rgb(33, 37, 41);
-	}
-	.title{
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		margin-top: 20px;
-		margin-bottom: 40px;
-	}
+  }
+  @media screen and (max-width: 600px) {
+    .nes {
+      width: 360px;
+    }
+  }
+  @font-face {
+    font-family: 'zpix';
+    src: url('./fonts/zpix-v1.2.1-legacy.ttf');
+  }
+  .root {
+    display: flex;
+    justify-content: center;
+    /* width: 100%; */
+  }
+  .nes {
+    font-family: 'zpix';
+    color: rgb(33, 37, 41);
+  }
+  .title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
 
-	/* @media (min-width: 640px) {
+  /* @media (min-width: 640px) {
 	} */
 </style>
--- eat-what-app/src/components/CardContainer.svelte
+++ eat-what-app/src/components/CardContainer.svelte
@@ -1,94 +1,97 @@
 <script>
-    import FoodSquare from "./FoodSquare.svelte";
-    import Controler from "./Controler.svelte";
+  import FoodSquare from './FoodSquare.svelte';
+  import Controler from './Controler.svelte';
 
-    let foods = [
-        {
-            icon: "https://img.icons8.com/color/48/000000/pizza-five-eighths.png",
-            name: "披萨",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/paella.png",
-            name: "香锅",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/hamburger.png",
-            name: "汉堡",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/chicken.png",
-            name: "炸鸡",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/greek-salad.png",
-            name: "健身餐",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/fondue.png",
-            name: "海底捞",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/noodles.png",
-            name: "辛拉面",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/white-sesame-seeds.png",
-            name: "炒饭",
-        },
-        {
-            icon: "https://img.icons8.com/color/48/000000/steak-medium.png",
-            name: "牛排",
-        },
-    ];
+  let foods = [
+    {
+      icon: 'https://img.icons8.com/color/48/000000/pizza-five-eighths.png',
+      name: '披萨',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/paella.png',
+      name: '香锅',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/hamburger.png',
+      name: '汉堡',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/chicken.png',
+      name: '炸鸡',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/greek-salad.png',
+      name: '健身餐',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/fondue.png',
+      name: '海底捞',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/noodles.png',
+      name: '辛拉面',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/white-sesame-seeds.png',
+      name: '炒饭',
+    },
+    {
+      icon: 'https://img.icons8.com/color/48/000000/steak-medium.png',
+      name: '牛排',
+    },
+  ];
 
-    let showFoods = foods;
-    let chance = 1;
+  let showFoods = foods;
+  let chance = 1;
 
-    //整体打乱
-    // function randomNumber() {
-    //     return 0.5 - Math.random();
-    // }
-    // function randomFood() {
-    //     foods.sort(randomNumber);
-    //     foods.sort(randomNumber);
-    //     foods.sort(randomNumber);
-    //     foods = [...foods];
-    // }
+  //整体打乱
+  // function randomNumber() {
+  //     return 0.5 - Math.random();
+  // }
+  // function randomFood() {
+  //     foods.sort(randomNumber);
+  //     foods.sort(randomNumber);
+  //     foods.sort(randomNumber);
+  //     foods = [...foods];
+  // }
 
-    function randomNumber() {
-        return Math.floor(Math.random() * 9);
+  function randomNumber() {
+    return Math.floor(Math.random() * 9);
+  }
+
+  function randomFood() {
+    let newFoods = [];
+    let fingerIcon =
+      'https://img.icons8.com/color/48/000000/one-finger--v1.png';
+    let pickedNum = randomNumber();
+    const rotate = [135, 180, 225, 90, 0, 270, 45, 0, 315];
+    for (let i = 0; i < 9; i++) {
+      newFoods.push({
+        icon: fingerIcon,
+        name: '',
+        rotate: rotate[i],
+      });
     }
-
-    function randomFood() {
-        let newFoods = [];
-        let fingerIcon =
-            "https://img.icons8.com/color/48/000000/one-finger--v1.png";
-        let pickedNum = randomNumber();
-        const rotate = [135, 180, 225, 90, 0, 270, 45, 0, 315];
-        for (let i = 0; i < 9; i++) {
-            newFoods.push({
-                icon: fingerIcon,
-                name: "",
-                rotate: rotate[i],
-            });
-        }
-        newFoods[4] = foods[pickedNum];
-        showFoods = newFoods;
-        chance = chance-1
-    }
+    newFoods[4] = foods[pickedNum];
+    showFoods = newFoods;
+    chance = chance - 1;
+  }
 </script>
 
 <section class="cardContainer">
-    <div class="nes-container">
-        <!-- <h3 class="title"></h3> -->
-        <FoodSquare {showFoods} {foods} />
-        <Controler bind:foods bind:chance {randomFood} />
-    </div>
+  <div class="nes-container outer-nes">
+    <!-- <h3 class="title"></h3> -->
+    <FoodSquare {showFoods} {foods} />
+    <Controler bind:foods bind:chance {randomFood} />
+  </div>
 </section>
 
 <style>
-    .cardContainer{
-        /* margin-bottom: 30px; */
-        margin: 30px 6px auto 6px;
-    }
+  .cardContainer {
+    /* margin-bottom: 30px; */
+    margin: 30px 6px auto 6px;
+  }
+  .outer-nes {
+    padding-bottom: 1px;
+  }
 </style>
