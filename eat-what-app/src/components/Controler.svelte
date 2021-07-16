be65ac0eef37673616038a9024b85de605397b2c
YUMING MA <DavidYangPersonal@outlook.com>
2021-07-16 13:35:07 +0800

    【UI】剩余次数文字颜色

--- eat-what-app/src/components/Controler.svelte
+++ eat-what-app/src/components/Controler.svelte
@@ -1,24 +1,24 @@
 <script>
-    // export let foods
-    export let randomFood;
-    export let chance;
-    let buttonState;
-    let buttonText;
-    $: buttonText = chance > 0 ? `随机` : `机会已用完`;
-    $: buttonState = chance > 0 ? "is-primary" : "is-disabled";
+  // export let foods
+  export let randomFood;
+  export let chance;
+  let buttonState;
+  let buttonText;
+  $: buttonText = chance > 0 ? `随机` : `机会已用完`;
+  $: buttonState = chance > 0 ? 'is-primary' : 'is-disabled';
 </script>
 
 <div class="controler">
-    <div>
-        <button
-            type="button"
-            class={`nes-btn ${buttonState}`}
-            disabled={buttonState == "is-disabled" ? true : false}
-            on:click={randomFood}>{buttonText}</button
-        >
-    </div>
-    <span>（剩余次数：{chance}）</span>
-    <!-- <div>
+  <div>
+    <button
+      type="button"
+      class={`nes-btn ${buttonState}`}
+      disabled={buttonState == 'is-disabled' ? true : false}
+      on:click={randomFood}>{buttonText}</button
+    >
+  </div>
+  <span class="chance-count">（剩余次数：{chance}）</span>
+  <!-- <div>
         <label>
             <input type="checkbox" class="nes-checkbox" checked />
             <span>辣的</span>
@@ -33,12 +33,15 @@
 </div>
 
 <style>
-    .controler {
-        margin-top: 18px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        /* height: 160px; */
-    }
+  .controler {
+    margin-top: 18px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    /* height: 160px; */
+  }
+  .chance-count {
+    color: white;
+  }
 </style>
